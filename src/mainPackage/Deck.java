03425e6e58ca0e005e07be39fb4e9c863f296c9d
mekbalidris <mekbalidriss@gmail.com>
2024-12-11 09:38:54 +0100

    created the deck function

--- src/mainPackage/Deck.java
+++ src/mainPackage/Deck.java
@@ -3,34 +3,49 @@ import java.util.Random;
 
 import mainPackage.Card.Color;
 import mainPackage.Card.Value;
+
+
 public class Deck {
     //is multiple of distinct cards
     //we have to create cards that are diffirent from each others and put it in an array
 
-    private Card[] cards = new Card[108];
-    private int CardInDeck;
-
+    private static Card[] cards = new Card[108];
     
+    
+        public static void main(){
+            for(int i=0; i<10; i++){
+            System.out.println(cards[i]);
+        }
+    }
 
     public Deck() {
-        Random random = new Random();
-        Card card ;
 
         for(int i=0; i<=3; i++) {
-            int randomIndex ;
-            do {
-                randomIndex = random.nextInt(109);
-            } while(cards[randomIndex] != null);
 
-            cards[randomIndex] = new Card(Color.getColors(i), Value.getValue(0));
+            int j = RandomIndex();
+
+            cards[j] = new Card(Color.getColors(i), Value.getValue(0));
             // Here we did the case of 0 value manualy becasue eache colore have one card of 0 value; 
             // *******************************************
             // *******************************************
 
-            for(int j=1; j <= 9; j++) {
-
+            for(int l=1; l <= 9; l++) {
+                for(int f=0; f<2; f++){
+                int k = RandomIndex();
+                cards[k] = new Card(Color.getColors(i), Value.getValue(l));
+                }
             } 
         }
+    }
+
+
+    int RandomIndex(){
+        Random random = new Random();
+        int i;
+        do {
+            i = random.nextInt(109);
+        } while(cards[i] != null);
+        return i;
     }
 
     
